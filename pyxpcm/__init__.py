02e781eb75696efbacb8be51139a7a0ada1a2525
Guillaume Maze <code@guillaumemaze.org>
2018-11-13 21:57:13 +0100

    Fix small error in init

--- pyxpcm/__init__.py
+++ pyxpcm/__init__.py
@@ -2,7 +2,7 @@
 .. moduleauthor:: Guillaume Maze <gmaze@ifremer.fr>
 """
 
-import pcm
-import dummy_datasets as datasets
+from . import pcm
 from . import stats
 from . import plot
+import dummy_datasets as datasets
